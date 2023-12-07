8eff42dea695433e843900f944c717452a052a0b
wjdtjddjq123 <59285584+wjdtjddjq123@users.noreply.github.com>
2023-12-07 21:06:31 +0900

    feat/ commit

--- java example/src/Main.java
+++ java example/src/Main.java
@@ -1,11 +1,17 @@
+<<<<<<< Updated upstream
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+=======
+import java.io.*;
+import java.util.Locale;
+>>>>>>> Stashed changes
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+<<<<<<< Updated upstream
         int N = Integer.parseInt(br.readLine()); // 좌석갯수
         String M = br.readLine();
         String Str = "";
@@ -27,4 +33,19 @@ public class Main {
         count = Math.min(count,N);
         System.out.println(count);
     }
+=======
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<str.length(); i++){
+                char c = str.charAt(i);
+                if(Character.isUpperCase(c)){
+                    sb.append(Character.toLowerCase(c));
+                }else{
+                    sb.append(Character.toUpperCase(c));
+                }
+            }
+            System.out.println(sb);
+
+        }
+>>>>>>> Stashed changes
 }
---
+++ java example/src/noviceArr/chapter04.java
@@ -0,0 +1,36 @@
+package noviceArr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class chapter04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder str = new StringBuilder();
+
+        // NxM의 행렬
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                arr[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                str.append(arr[i][j] + Integer.parseInt(st.nextToken())).append(" ");
+            str.append("\n");
+        }
+
+        System.out.print(str);
+        br.close();
+    }
+}
